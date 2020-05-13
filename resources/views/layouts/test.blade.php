3bd30d80242b635ea7723df5c3f0dc8feecea466
unknown <jaffar.jatmiko.jati@mail.ugm.ac.id>
2020-05-13 21:04:01 +0700

    post

--- resources/views/layouts/test.blade.php
+++ resources/views/layouts/test.blade.php
@@ -21,6 +21,7 @@ https://templatemo.com/tm-535-softy-pinko
     <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/all.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/templatemo-softy-pinko.css">
     
 
@@ -49,19 +50,21 @@ https://templatemo.com/tm-535-softy-pinko
                         <a href="#" class="logo">
                             <img src="assets/images/logo1.png" alt="Softy Pinko" style="width:10em;"/>
                         </a>
+                        <div class="search-wrapper search">
+                            <div class="input-holder">
+                                <input type="text" class="search-input" placeholder="Type to search">
+                                <button class="search-icon"><span></span></button>
+                            </div>
+                            <button class="close"></button>
+                        </div>
                         <!-- ***** Logo End ***** -->
                         <!-- ***** Menu Start ***** -->
                         <ul class="nav">
-                            <li><a href="#welcome" class="active">Home</a></li>
-                            <li><a href="#features">About</a></li>
-                            <li><a href="#work-process">Work Process</a></li>
-                            <li><a href="#testimonials">Testimonials</a></li>
-                            <li><a href="#pricing-plans">Pricing Tables</a></li>
-                            <li><a href="#blog">Blog Entries</a></li>
-                            <li><a href="#contact-us">Contact Us</a></li>
+                            <li><a href="#welcome" class="active">Beranda</a></li>
+                            <li><a href="#features">Penggalangan Dana</a></li>
+                            <li><a href="/login" class="main-login">Masuk</a></li>
                             <li><a href="{{ route('logout') }}" onclick="event.preventDefault();
                           document.getElementById('logout-form').submit();">{{ __('Logout') }}</a></li>
-                            
                         </ul>
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
@@ -85,11 +88,11 @@ https://templatemo.com/tm-535-softy-pinko
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12">
                     <ul class="social">
-                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
                         <li><a href="#"><i class="fa fa-rss"></i></a></li>
-                        <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                        <li><a href="#"><i class="fab fa-dribbble"></i></a></li>
                     </ul>
                 </div>
             </div>
@@ -116,6 +119,6 @@ https://templatemo.com/tm-535-softy-pinko
     
     <!-- Global Init -->
     <script src="assets/js/custom.js"></script>
-
+    <script type="text/javascript" src="{{ asset('assets') }}/scripts/main.js"></script></body>
   </body>
 </html>
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -10,12 +10,103 @@
                 <h1><strong>Pembangunan Gedung Baru Pondok pesantren Tebu Ireng</strong></h1>
             </div>
         </div>
-        <div class="col-md-8">
-            <p>amjay</p>
+        <div class="col-lg-8 col-md-6 col-sm-12">
+            <div class="item-post">
+                <div class="post-pict-item">
+                    <img src="assets/images/eduard.jpg" class="rounded img-fluid d-block mx-auto" alt="">
+                </div>
+                <div class="admin-item-post">
+                    <div class="row">
+                        <div class="col-lg-2 col-md-6 col-sm-12">
+                            <img src="assets/images/avatars/5.jpg" alt="">
+                        </div>
+                        <div class="col-lg-10 col-md-6 col-sm-12 admin-item">
+                            <p>Jaffar Jatmiko Jati adalah yang mengelola penggalangan dana ini</p>
+                        </div>
+                    </div>
+                    <div class="date-item">
+                        <p>Dibuat pada 13 oktober 2019</p>
+                    </div>
+                    <div class="desc-item">
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+                        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+                        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+                        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+                        officia deserunt mollit anim id est laborum.<br>
+                        Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo pharetra, 
+                        est eros bibendum elit, nec luctus magna felis sollicitudin mauris. Integer in mauris eu nibh euismod 
+                        gravida. Duis ac tellus et risus vulputate vehicula. Donec lobortis risus a elit. Etiam tempor. Ut 
+                        ullamcorper, ligula eu tempor congue, eros est euismod turpis, id tincidunt sapien risus a quam. 
+                        Maecenas fermentum consequat mi. Donec fermentum. Pellentesque malesuada nulla a mi. Duis sapien sem, 
+                        aliquet nec, commodo eget, consequat quis, neque. Aliquam faucibus, elit ut dictum aliquet, felis nisl 
+                        adipiscing sapien, sed malesuada diam lacus eget erat. Cras mollis scelerisque nunc. Nullam arcu. 
+                        Aliquam consequat. Curabitur augue lorem, dapibus quis, laoreet et, pretium ac, nisi. Aenean magna nisl, 
+                        mollis quis, molestie eu, feugiat in, orci. In hac habitasse platea dictumst.
+                        </p>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-6 col-md-6 col-sm-12 text-center padding-bottom-40 padding-top-20">
+                            <a href="#" class="donate-button">Donasi Sekarang</a>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-12 text-center padding-bottom-40 padding-top-20">
+                            <a href="#" class="share-button">Bagikan</a>
+                        </div>
+                        <div class="col-lg-12 col-md-12 col-sm-12 border-bottom-20">
+                            <h5>Pengelola</h5>
+                        </div>
+                        <div class="col-lg-2 col-md-4 col-sm-12 padding-bottom-40 padding-top-20">
+                            <img src="assets/images/avatars/5.jpg" alt="">
+                        </div>
+                        <div class="col-lg-6 col-md-4 col-sm-6  padding-bottom-40 padding-top-20 admin-item">    
+                            <p>Jaffar Jatmiko Jati<br>Pengelola</p>
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-6  padding-bottom-40 padding-top-20 admin-item right-text">    
+                            <a href="#" class="contact-button">Kontak</a>
+                        </div>
+                        <div class="col-lg-12 col-md-12 col-sm-12 border-bottom-20">
+                            <h5>Komentar</h5>
+                        </div>
+                        <div class="col-lg-12 col-md-12 col-sm-12 padding-top-20 border-bottom-20">
+                            <div class="row">
+                                <div class="col-lg-2 col-md-4 col-sm-4">
+                                    <div class="col-lg-12 col-md-12 col-sm-12 loyalty-item">
+                                        <i class="far fa-user-circle icon-gradient bg-grow-early"></i>
+                                    </div>
+                                </div>
+                                <div class="col-lg-10 col-md-8 col-sm-8">
+                                    <p>Luhut Pandjaitan donasi <b>Rp. 10.000</b></p>
+                                    <p>Semoga bermanfaat ya :)</p>
+                                    <br>
+                                    <p class="lastdonate">1 jam yang lalu</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-12 col-md-12 col-sm-12 padding-top-20 border-bottom-20">
+                            <div class="row">
+                                <div class="col-lg-2 col-md-4 col-sm-4">
+                                    <div class="col-lg-12 col-md-12 col-sm-12 loyalty-item">
+                                        <i class="far fa-user-circle icon-gradient bg-grow-early"></i>
+                                    </div>
+                                </div>
+                                <div class="col-lg-10 col-md-8 col-sm-8">
+                                    <p>Luhut Pandjaitan donasi <b>Rp. 10.000</b></p>
+                                    <p>Semoga bermanfaat ya :)</p>
+                                    <br>
+                                    <p class="lastdonate">1 jam yang lalu</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-12 col-md-12 col-sm-12 padding-top-20">
+                            <i class="far fa-flag"></i>&nbsp<a href="#" class="report">Laporkan Penggalangan</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="col-lg-4 col-md-6 col-sm-12">
-            <div class="post-item">
-                <div class="post-header">
+        <div class="col-lg-4 col-md-6 col-sm-12 sidebar-item">
+            <div class="sidebar-donate-item">
+                <div class="sidebar-donate-header">
                     <h3><strong>Rp. 80.000.000 </strong></h3>
                     <p>terkumpul dari Rp. 400.000.000</p>
                     <div class="progress-bar-xs progress">
@@ -23,11 +114,35 @@
                     </div>
                     <p><strong>20%</strong> tercapai</p>
                 </div>
-                <div class="post-body">
+                <div class="sidebar-donate-body">
                 <a href="#" class="donate-button">Donasi Sekarang</a>
                 </div>
-                <div class="post-footer">
-                    <a href="#" class="main-button">Purchase Now</a>
+                <div class="sidebar-donate-footer">
+                    <a href="#" class="share-button">Bagikan</a>
+                </div>
+                <div class="col-lg-12 col-md-12 col-sm-12 padding-top-20 border-bottom-20">
+                    <div class="row">
+                        <div class="col-lg-2 col-md-4 col-sm-4 padding-0">
+                            <div class="col-lg-12 col-md-12 col-sm-12 loyalty-item">
+                                <i class="far fa-user-circle icon-gradient bg-grow-early"></i>
+                            </div>
+                        </div>
+                        <div class="col-lg-10 col-md-8 col-sm-8">
+                            <p>Luhut Pandjaitan</p>
+                            <div class="row padding-0">
+                                <div class="col-lg-6 col-md-5 col-sm-5">
+                                    <p><b>Rp. 10.000</b></p>        
+                                </div>
+                                <div class="col-lg-1 col-md-1 col-sm-1">
+                                    <i class="fas fa-circle" style="font-size: 6px;"></i>        
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-sm-4">
+                                    <p>jancok</p>                
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
