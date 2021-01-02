2dbd6f7ecc828e535303f34ec6ab47d53daab7af
unknown <jaffar.jatmiko.jati@mail.ugm.ac.id>
2021-01-03 00:57:21 +0700

    delete dan tabel kegagalan

---
+++ .history/app/Http/Controllers/AdminController_20201231011920.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Galadana;
+
+class AdminController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('admin.index');
+    }
+    public function managepost()
+    {
+        $galadana= Galadana::all();
+        $galadana = galadana::join('users', 'users.id','=', 'galadana.user_id')
+            ->select('users.*', 'galadana.*')
+            ->orderBy('galadana.created_at','desc')
+            ->paginate(12);
+        return view ('admin.post.index', compact('galadana'));
+    }
+    public function approvalpost()
+    {
+        $galadana= Galadana::all();
+        $galadana = galadana::join('users', 'users.id','=', 'galadana.user_id')
+            ->select('users.*', 'galadana.*')
+            ->orderBy('galadana.created_at','desc')
+            ->paginate(9);
+        return view ('admin.post.approval', compact('galadana'));
+    }
+    public function declinepost()
+    {
+        $galadana= Galadana::all();
+        $galadana = galadana::join('users', 'users.id','=', 'galadana.user_id')
+            ->select('users.*', 'galadana.*')
+            ->orderBy('galadana.created_at','desc')
+            ->paginate(9);
+        return view ('admin.post.declines', compact('galadana'));
+    }
+    public function approve($id)
+    {
+        $galadana = Galadana::findOrFail($id);
+        $galadana->status = 1;
+        $galadana->save();
+        return redirect()->back()->withStatus(__('approve'));
+    }
+    public function decline($id)
+    {
+        $galadana = Galadana::findOrFail($id);
+        $galadana->status = 0;
+        $galadana->save();
+        return redirect()->back()->withStatus(__('decline'));
+    }
+    public function test()
+    {
+        return view ('admin.test');
+    }
+}
---
+++ .history/resources/views/admin/post/edit.blade_20210101000612.php
@@ -0,0 +1,60 @@
+@extends('layouts.dashboard', [
+    'namePage' => 'post',
+    'activePage' => 'post',
+])
+@section('content')
+    <div class="app-main__inner">
+        <div class="app-page-title">
+            <div class="page-title-wrapper">
+                <div class="page-title-heading">
+                    <div class="page-title-icon">
+                        <i class="pe-7s-car icon-gradient bg-mean-fruit">
+                        </i>
+                    </div>
+                    <div>Analytics Dashboard
+                        <div class="page-title-subheading">This is an example dashboard created using build-in elements and components.
+                        </div>
+                    </div>
+                </div>
+                <div class="page-title-actions">
+                    <button type="button" data-toggle="tooltip" title="Example Tooltip" data-placement="bottom" class="btn-shadow mr-3 btn btn-dark">
+                        <i class="fa fa-star"></i>
+                    </button>
+                    <button type="button" aria-haspopup="true" aria-expanded="false" class="btn btn-info">
+                        <span class="btn-icon-wrapper pr-2 opacity-7">
+                            <i class="fa fa-business-time fa-w-20"></i>
+                        </span>
+                        Buat Kategori
+                    </button>
+                </div>    </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="main-card mb-3 card">
+                    <div class="card-body"><h5 class="card-title">Control Types</h5>
+                        <form id="createForm" method="POST" action="{{ route('manajemen-kategori.update', $galadana->id) }}" enctype="multipart/form-data">
+                        @csrf
+                        @method('put')
+                            <div class="form-group{{ $errors->has('gambar') ? ' has-danger' : '' }} position-relative">
+                                <label class="form-control-label" for="gambar">{{ __('Gambar *  ') }}</label>
+                                <fieldset>
+                                    <input name="gambar" id="gambar" type="file" class="form-control-file" value="{{old('gambar')}}" required="">
+                                </fieldset>
+                                @include('alerts.feedback', ['field' => 'gambar'])
+                            </div>
+                            
+                            <div class="form-group{{ $errors->has('judul') ? ' has-danger' : '' }} position-relative">
+                                <label class="form-control-label" for="input-judul">{{ __('Judul *  ') }}</label>
+                                <fieldset>
+                                    <input name="judul" value="{{ old('name', $galadana->judul) }}" type="text" class="form-control" id="judul">
+                                </fieldset>
+                                @include('alerts.feedback', ['field' => 'judul'])
+                            </div>
+                            <button class="mt-1 btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
