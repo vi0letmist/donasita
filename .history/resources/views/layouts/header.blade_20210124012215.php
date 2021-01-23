373b8de1506d10dbad11d9054ca201c0f9ff904f
unknown <jaffar.jatmiko.jati@mail.ugm.ac.id>
2021-01-24 02:14:33 +0700

    sedikit nav

---
+++ .history/resources/views/layouts/header.blade_20210124012215.php
@@ -0,0 +1,34 @@
+<nav class="main-nav nav-page">
+    <ul class="nav nav-page-left">
+        <li>
+            <div class="search-wrapper search">
+                <div class="input-holder">
+                    <input type="text" class="search-input" placeholder="Type to search">
+                    <button class="search-icon"><span></span></button>
+                </div>
+                <button class="close"></button>
+            </div>
+        </li>
+        <li><a href="/galadana">Galadana untuk</a></li>
+    </ul>
+    <!-- ***** Logo Start ***** -->
+    <div class="nav-page-center">
+    <a href="/home" class="logo">
+        <img src="{{ asset('assets') }}/images/logo1.png" alt="Softy Pinko" style="width:10em;"/>
+    </a>
+    </div>
+    <!-- ***** Logo End ***** -->
+    <!-- ***** Menu Start ***** -->
+    <ul class="nav nav-page-right">
+        <li><a href="/login">Masuk</a></li>
+        <li><a href="{{ route('logout') }}" onclick="event.preventDefault();
+      document.getElementById('logout-form').submit();">{{ __('Logout') }}</a></li>
+    </ul>
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+    <a class='menu-trigger'>
+        <span>Menu</span>
+    </a>
+    <!-- ***** Menu End ***** -->
+</nav>
--- .history/resources/views/layouts/sticky-navbar.blade_20210124011804.php
+++ .history/resources/views/layouts/sticky-navbar.blade_20210124011804.php
@@ -17,13 +17,14 @@ https://templatemo.com/tm-535-softy-pinko
 
     <!-- Additional CSS Files -->
     <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/css/bootstrap.min.css">
-    
+
     <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/css/font-awesome.css">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
     <link href="{{ asset('css/all.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('assets') }}/css/templatemo-softy-pinko.css">
-    
+    <link href="{{ asset('assets') }}/css/datatables.min.css" rel="stylesheet">
 
     </head>
     
@@ -41,12 +42,13 @@ https://templatemo.com/tm-535-softy-pinko
     
     
     <!-- ***** Header Area Start ***** -->
-    <header class="header-area2 header-sticky">
+    <header class="header-area header-sticky">
         <div class="container">
             <div class="row">
                 <div class="col-12">
                     <!-- ***** Footer Start ***** -->
                         @include('layouts.header')
+                    
                 </div>
             </div>
         </div>
@@ -58,23 +60,50 @@ https://templatemo.com/tm-535-softy-pinko
     <!-- ***** Footer Start ***** -->
     @include('layouts.footer')
     
+    
     <!-- jQuery -->
     <script src="{{ asset('assets') }}/js/jquery-3.5.1.js"></script>
     <script src="{{ asset('assets') }}/js/app.js"></script>
+
     <!-- Bootstrap -->
     <script src="{{ asset('assets') }}/js/popper.js"></script>
     <script src="{{ asset('assets') }}/js/bootstrap.min.js"></script>
 
     <!-- Plugins -->
     <script src="{{ asset('assets') }}/js/scrollreveal.min.js"></script>
-    <script src="{{ asset('assets') }}/js/waypoints.min.js"></script>
     <script src="{{ asset('assets') }}/js/jquery.counterup.min.js"></script>
     <script src="{{ asset('assets') }}/js/imgfix.min.js"></script> 
     
     <!-- Global Init -->
     <script src="{{ asset('assets') }}/js/custom.js"></script>
     <script type="text/javascript" src="{{ asset('assets') }}/scripts/main.js"></script>
-
+    @stack('js')
+    @include('sweetalert::alert')
+    <script type="text/javascript" src="{{ asset('assets') }}/js/sweetalert.min.js"></script>
+    <script type="text/javascript" src="{{ asset('assets') }}/js/datatables.min.js"></script>
     <script src="{{ asset('ckeditor') }}/ckeditor.js"></script>
+    <script type="text/javascript">
+        CKEDITOR.replace( 'cerita',{
+            filebrowserUploadUrl: "{{route('upload', ['_token' => csrf_token() ])}}",
+            filebrowserUploadMethod: 'form'
+        });
+    </script>
+    <script>
+        $(document).ready(function() {
+            $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
+                });
+            
+            $('.collapse').collapse('hide');
+        });
+        $('#myTab a').on('click', function (e) {
+                e.preventDefault()
+                $(this).tab('show')
+            });
+    </script>
+    
+
   </body>
 </html>
